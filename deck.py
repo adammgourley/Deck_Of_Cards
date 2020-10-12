cbbf39651cc7da09f568cf0cc8078e94ce0baadb
Adam <Adam Gourley>
2020-10-12 16:15:01 -0400

    On branch master Initial commit Changes to be committed: 	new file:   deck.py

---
+++ deck.py
@@ -0,0 +1,23 @@
+# deck.py
+
+import random
+
+
+class Deck:
+	suits = ["Hearts", "Clubs", "Spades", "Diamonds"]
+	values = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"]
+
+	def __init__(self):
+		self.deck = []
+		self.get_deck()
+
+
+	def get_deck(self):
+		for s in Deck.suits:
+			for v in Deck.values:
+				self.deck.append(f"{str(v)} of {s}")
+		return self.shuffle(self.deck)
+
+	def shuffle(self, deck):
+		random.shuffle(deck)
+		return deck
